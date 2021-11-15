e51a4a8f79a5cf81afeafc9cccb5ec200ef39451
Dika muharman <dika1254@gmail.com>
2021-11-16 05:21:07 +0700

    create table patient and status patient

---
+++ database/migrations/2021_11_15_220258_create_patients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\StatusPatient;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->text('alamat');
+            $table->foreignIdFor(StatusPatient::class);
+            $table->date('in_date_at');
+            $table->date('out_date_at')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
